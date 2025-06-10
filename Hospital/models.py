4341d0510dd49dc7a0d2853297a8efe593981996
Unnikrishnan <unnioffi0620@gmail.com>
2025-06-10 10:11:35 +0530

    Initialised Hospital Project and Created Home and LoginFunctionality, Created Credentials Model for storing user values

--- Hospital/models.py
+++ Hospital/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Credentials(models.Model):
+    username = models.CharField(max_length=100,unique=True, default="Unknown")
+    name = models.CharField(max_length=255, default="Unknown")
+    email = models.EmailField(max_length=255,unique=True, default="Unknown")
+    phone = models.CharField(max_length=12,default="Unknown")
+    password = models.CharField(max_length=255, default="Unknown")
+    type=models.CharField(max_length=50,default="Unknown")
+    def __str__(self):
+        return self.username
--- Hospital/views.py
+++ Hospital/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from . import urls
+import bcrypt
+from Hospital.models import *
+from django.contrib import messages
 # Create your views here.
+
+def homePage(request):
+    return render(request, 'homePage.html')
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = Credentials.objects.filter(username=username).first()
+        if user is not None:
+            if bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
+                if user.type == "Patient":
+                    request.session['user_id'] = user.id
+                    return redirect('userprofile')
+                elif user.type == "Doctor":
+                    request.session['user_id'] = user.id
+                    return redirect('doctorprofile')
+                elif user.type=="Admin":
+                    request.session['user_id'] = user.id
+                    return redirect('adminprofile')
+            else:
+                messages.error(request,"Invalid Password!")
+                return redirect('login')
+        else:
+            messages.error(request,"Invalid User!")
+            return redirect('login')
+    return render(request,'loginPage.html')
+def logout(request):
+    request.session.flush()
+    return redirect('login')
