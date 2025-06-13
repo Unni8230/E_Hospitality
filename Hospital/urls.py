1768892e45347d89b560f5ecf07a11271e68a8a3
Unnikrishnan <unnioffi0620@gmail.com>
2025-06-13 20:46:49 +0530

    Implemented logout functionality by flushing out current session

--- Hospital/urls.py
+++ Hospital/urls.py
@@ -3,6 +3,7 @@ from . import views
 urlpatterns = [
    path('',views.homePage,name='home'),
    path('login/',views.loginPage,name='login'),
+   path('logout/',views.logout,name='logout'),
    path('register/',views.registerUser,name='register'),
    path('userprofile/',views.userProfile,name='userprofile'),
    path('doctorprofile/',views.doctorProfile,name='doctorprofile'),
